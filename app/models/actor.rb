de021e5eb5a194e347159b0c9b7078d9ad3eec05
mjones1818 <michael.jones0532@gmail.com>
2020-11-03 15:42:25 -0600

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
+require 'pry'
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    results = []
+    self.characters.each_with_index do |character, index|
+      results << "#{character.name} - #{self.characters[index].show.name}"
+    end
+    results
+  end
 end
